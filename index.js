0c18a4daa5b352d85fb222c334de4f35e7ab0ff5
cnFuwa <cnliufu@outlook.com>
2024-09-01 08:57:16 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-
-var app = express();
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const multer = require('multer');
+const upload = multer({dest: 'uploads/'});
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+	res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
-
+app.post('/api/fileanalyse', upload.single('upfile'),
+	function (req, res, next) {
+		const file_info = req.file;
+		console.log(file_info);
+		res.json({
+			name: file_info.originalname,
+			type: file_info.mimetype,
+			size: file_info.size
+		});
+	});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+	console.log('Your app is listening on port ' + port)
 });
